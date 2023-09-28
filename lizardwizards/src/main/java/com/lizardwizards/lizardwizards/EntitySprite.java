46c9e2c25d3398ae453ae62442c950da9aa8c91a
Faustas Linkevičius <faulin@ktu.lt>
2023-09-28 21:14:50 +0300

    Added sprite size

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/EntitySprite.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/EntitySprite.java
@@ -5,8 +5,10 @@ import javafx.scene.shape.Rectangle;
 
 //Rectangle is temporary
 public class EntitySprite extends Rectangle {
-    EntitySprite(Vector2 position)
+    EntitySprite(Vector2 position, Vector2 size)
     {
+        this.setHeight(size.x);
+        this.setWidth(size.y);
         setTranslateX(position.x);
         setTranslateY(position.y);
     }
