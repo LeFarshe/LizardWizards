e2429035c1f148684cf4bbae41f2f96586ea38e3
Faustas Linkevičius <faulin@ktu.lt>
2023-09-28 21:16:03 +0300

    Added sprites and fixed movement

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/Player.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/Player.java
@@ -3,6 +3,7 @@ package com.lizardwizards.lizardwizards;
 public class Player {
     Vector2 position;
     Vector2 moveDirection = new Vector2(0,0);
+    EntitySprite sprite;
     double speed;
     boolean isMoving = false;
 
@@ -15,13 +16,17 @@ public class Player {
     //Delta in seconds
     public void Move(double delta)
     {
-        Vector2 directionTemp = moveDirection.Copy();
-        position.AddVector(directionTemp.Multiply(speed * delta));
+        if (isMoving)
+        {
+            Vector2 directionTemp = moveDirection.Copy();
+            position.AddVector(directionTemp.Multiply(speed * delta));
+            sprite.Move(position);
+        }
     }
 
     public void StartMoving(Vector2 direction)
     {
-        if (direction.x == 0 && direction.x == 0)
+        if (direction.x == 0 && direction.y == 0)
         {
             StopMoving();
             return;
