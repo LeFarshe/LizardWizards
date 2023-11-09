97485c4e1a82335da889604868e90b8ab0ce8726
Faustas Linkevičius <faulin@ktu.lt>
2023-11-09 15:41:01 +0200

    GenerationRoomData class used only when building levels

---
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/GenerationRoomData.java
@@ -0,0 +1,11 @@
+package com.lizardwizards.lizardwizards.core.gameplay;
+
+public class GenerationRoomData {
+    RoomData roomData;
+
+    // 1 should be default
+    double probability;
+
+    // Can the side have a door
+    boolean up, right, down, left;
+}
