e2ca39dc5c60ec88a0880c9efaf89a4def3fce23
Faustas Linkevičius <faulin@ktu.lt>
2023-11-13 21:05:41 +0200

    Added a function to check if a point is in a line and a function to get the slope and intercept of the line

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/collision/Line.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/collision/Line.java
@@ -2,17 +2,20 @@ package com.lizardwizards.lizardwizards.core.gameplay.collision;
 
 import com.lizardwizards.lizardwizards.core.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     Vector2 start;
     Vector2 end;
 
     public Line(Vector2 start, Vector2 end){
-        this.start = start;
-        this.end = end;
+        this.start = start.Copy();
+        this.end = end.Copy();
     }
 
     public Line(Vector2 start, Vector2 direction, double length){
-        this.start = start;
+        this.start = start.Copy();
         this.end = start.Copy().AddVector(direction.Copy().Normalize().Multiply(length));
     }
 
@@ -22,5 +25,60 @@ public class Line {
 
     public double getLength(){
         return start.DistanceTo(end);
+    }
+
+    /**
+     * Returns the slope (0) and intercept (1) of this line.
+     * If the slope is infinite, then it will return null and the X value
+     */
+    public List<Double> getSlopeAndIntercept(){
+        List<Double> ans = new ArrayList<>();
+        if (start.x == end.x) {
+            ans.add(null);
+            ans.add(start.x);
+        }
+        else {
+            if (start.y == end.y) {
+                ans.add((double)0);
+                ans.add(start.y);
+            }
+            else {
+                ans.add((end.y - start.y) / (end.x - start.x));
+                ans.add(start.y - (start.x * ans.get(0)));
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * Checks if a point exists in this finite line when this point exists in an infinite version of this line
+     */
+    public boolean pointIsInFiniteLine(Vector2 point){
+        if (start.x != end.x){
+            double xMin, xMax;
+            if (start.x < end.x){
+                xMin = start.x;
+                xMax = end.x;
+            }
+            else {
+                xMin = end.x;
+                xMax = start.x;
+            }
+
+            return point.x >= xMin && point.x <= xMax;
+        }
+        else {
+            double yMin, yMax;
+            if (start.y < end.y){
+                yMin = start.y;
+                yMax = end.y;
+            }
+            else {
+                yMin = end.y;
+                yMax = start.y;
+            }
+
+            return point.y >= yMin && point.y <= yMax;
+        }
     }
 }
