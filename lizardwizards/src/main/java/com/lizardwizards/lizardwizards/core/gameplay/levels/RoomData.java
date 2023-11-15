78c737bbca03fe97840cd5bdb8eee4d373efb88a
Faustas Linkevičius <faulin@ktu.lt>
2023-11-15 15:52:59 +0200

    RoomData is now cloned

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/levels/RoomData.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/levels/RoomData.java
@@ -1,6 +1,6 @@
 package com.lizardwizards.lizardwizards.core.gameplay.levels;
 
-public class RoomData {
+public class RoomData implements Cloneable{
     RoomEnumerator id;
     boolean cleared;
 
@@ -8,4 +8,16 @@ public class RoomData {
         this.id = id;
     }
     public void setCleared() { cleared = true;}
+
+    @Override
+    public RoomData clone() {
+        try {
+            RoomData clone = (RoomData) super.clone();
+            clone.cleared = cleared;
+            clone.id = id;
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
