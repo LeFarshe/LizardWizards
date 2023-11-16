ce3f1c7225221e4f52a6dd21a5525007e6a032f6
Julius <julius@supremits.lt>
2023-11-17 00:27:03 +0200

    Add strategy pattern explicitly

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/DefaultEnemyFactory.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/DefaultEnemyFactory.java
@@ -10,6 +10,7 @@ public class DefaultEnemyFactory implements IEnemyFactory {
 
     public IEnemy createEnemy(Vector2 position, int health) {
         EnemyEntity enemyEntity = new EnemyEntity();
+        enemyEntity.setMovementStrategy(new SimpleMovementStrategy());
         enemyEntity.SetPosition(position);
         IEnemyImplementor implementor = new StandardEnemyImplementor(enemyEntity, position, 100, health);
         return new StandardEnemy(implementor, health, position);
--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/EnemyEntity.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/EnemyEntity.java
@@ -9,23 +9,24 @@ import java.util.Dictionary;
 import java.util.Random;
 
 public class EnemyEntity extends Entity {
-    Vector2 moveDirection = new Vector2(0,0);
+
+    EnemyMovementStrategy strategy;
     int health = 3;
     double speed;
     double directionDelay = 1;
     double directionTimer = directionDelay;
     boolean isDestroyed = false;
-    private final Random rand = new Random();
     @Override
     public void MoveByDelta(double delta) {
         directionTimer -= delta;
         if (directionTimer <= 0){
-            setRandomDirection();
             directionTimer = directionDelay;
         }
-        Move(moveDirection.Copy().Multiply(speed * delta));
+        Move(strategy.getDirection().Copy().Multiply(speed * delta));
     }
-
+    void setMovementStrategy(EnemyMovementStrategy strategy) {
+        this.strategy = strategy;
+    }
     @Override
     public void Collide(Entity collider, CollisionLayer layer) {
         final CollisionLayer PLAYER_PROJECTILE_LAYER = CollisionLayer.PlayerProjectile;
@@ -40,11 +41,7 @@ public class EnemyEntity extends Entity {
     }
 
 
-    public void setRandomDirection() {
-        double randX = rand.nextDouble() * 2 - 1;
-        double randY = rand.nextDouble() * 2 - 1;
-        moveDirection = new Vector2(randX, randY).Normalize();
-    }
+
 
     public void HandleDeath() {
         isDestroyed = true;
