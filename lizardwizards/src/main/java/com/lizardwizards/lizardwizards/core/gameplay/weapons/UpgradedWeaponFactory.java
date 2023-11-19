1127995be1b1798ec284b9f58bee2459018d674f
Faustas Linkevičius <faulin@ktu.lt>
2023-11-19 23:34:15 +0200

    Updated the Upgraded weapon factory

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/weapons/UpgradedWeaponFactory.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/weapons/UpgradedWeaponFactory.java
@@ -1,32 +1,38 @@
 package com.lizardwizards.lizardwizards.core.gameplay.weapons;
 
-import com.lizardwizards.lizardwizards.core.gameplay.projectiles.ProjectileDecorators;
+import com.lizardwizards.lizardwizards.core.gameplay.weapons.decorators.*;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
 public class UpgradedWeaponFactory extends WeaponFactory {
     int upgradeAmount;
+    static Random random = new Random();
     public UpgradedWeaponFactory(int upgradeAmount){
         this.upgradeAmount = upgradeAmount;
     }
 
     @Override
-    public Weapon getWeapon(WeaponTypes weapon){
+    public IWeapon getWeapon(WeaponTypes weapon){
         return randomlyDecorate(super.getWeapon(weapon));
     }
 
-    private Weapon randomlyDecorate(Weapon weapon) {
-        Random rand = new Random();
-        LinkedList<ProjectileDecorators> projectileDecorators = new LinkedList<>(List.of(
-                ProjectileDecorators.Spectral,
-                ProjectileDecorators.Aggrevator,
-                ProjectileDecorators.DoubleCaliber,
-                ProjectileDecorators.TimeBullets
-        ));
-        for (int i =0; i< upgradeAmount; i++){
-            weapon.DecorateProjectile(projectileDecorators.get(rand.nextInt(projectileDecorators.size())));
+    private IWeapon randomlyDecorate(IWeapon weapon) {
+        List<WeaponDecorators> weaponDecorators = List.of(WeaponDecorators.Aggravator,
+                WeaponDecorators.DoubleCaliber,
+                WeaponDecorators.DoubleShots,
+                WeaponDecorators.Spectral,
+                WeaponDecorators.TenetShots);
+        for (int i = 0; i < upgradeAmount; i++){
+            switch(weaponDecorators.get(random.nextInt(weaponDecorators.size()))){
+                case Aggravator -> weapon = new Aggravator(weapon);
+                case DoubleCaliber -> weapon = new DoubleCaliber(weapon);
+                case DoubleShots -> weapon = new DoubleShots(weapon);
+                case Spectral -> weapon = new Spectral(weapon);
+                case TenetShots -> weapon = new TenetShots(weapon);
+            }
         }
         return weapon;
     }
