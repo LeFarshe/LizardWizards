d0ca5893b57cf06562bf2f131a214a4d7766e090
Faustas Linkevičius <faulin@ktu.lt>
2023-11-22 17:35:25 +0200

    Removed useless decorators and factories

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/weapons/decorators/WeaponDecorators.java
+++
@@ -1,9 +0,0 @@
-package com.lizardwizards.lizardwizards.core.gameplay.weapons.decorators;
-
-public enum WeaponDecorators {
-    Aggravator,
-    DoubleCaliber,
-    DoubleShots,
-    Spectral,
-    TenetShots
-}
