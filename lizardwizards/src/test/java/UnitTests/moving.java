1593289a73812009a771f8864fbc4938659ffb8c
Povilas <povilassc@gmail.com>
2023-12-11 00:28:08 +0200

    finished this

--- lizardwizards/src/test/java/UnitTests/moving.java
+++ lizardwizards/src/test/java/UnitTests/moving.java
@@ -5,15 +5,24 @@ import com.lizardwizards.lizardwizards.core.gameplay.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class moving {
     @Test
     void playerMovingTest(){
+        double[][] tests = {
+            {0,0,100,1,0,0.02,2,0},
+            {540,200,-500,0.707,0.707,0.05,522.322,182.322},
+            {0.001,-0.001,147,0,-1,0.01,0.001,-1.471},
+            {231,10,200,0,0,0.1,231,10},
+            {0,150000,100000000000.0,0,1,0.03,0,3000150000.0}
+        };
 
-
-        var player = new Player(new Vector2(0, 0), 100);
-        player.StartMoving(new Vector2(1, 1));
-        player.MoveByDelta(0.01);
-        assertEquals(player.GetPosition().DistanceTo(new Vector2(1, 1)), 0);
+        for (var i : tests){
+            var player = new Player(new Vector2(i[0], i[1]), i[2]);
+            player.StartMoving(new Vector2(i[3], i[4]));
+            player.MoveByDelta(i[5]);
+            assertTrue(player.GetPosition().DistanceTo(new Vector2(i[6], i[7])) < 0.005);
+        }
     }
 }
