1bb798f95db321e052ce887cd15a98a985edd62d
Povilas <povilassc@gmail.com>
2023-12-13 03:05:16 +0200

    Added a help command

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/server/ServerCommands.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/server/ServerCommands.java
@@ -3,7 +3,6 @@ package com.lizardwizards.lizardwizards.server;
 import com.lizardwizards.lizardwizards.client.sprites.EntitySprite;
 import com.lizardwizards.lizardwizards.core.Vector2;
 import com.lizardwizards.lizardwizards.core.communication.RoomInformation;
-import com.lizardwizards.lizardwizards.core.gameplay.Entity;
 import com.lizardwizards.lizardwizards.core.gameplay.EntityWrapper;
 import com.lizardwizards.lizardwizards.core.gameplay.collision.Collider;
 import com.lizardwizards.lizardwizards.core.gameplay.collision.CollisionLayer;
@@ -13,12 +12,30 @@ import com.lizardwizards.lizardwizards.core.gameplay.items.ItemHolder;
 import com.lizardwizards.lizardwizards.core.gameplay.items.items.WeaponUpgradeItem;
 import com.lizardwizards.lizardwizards.core.gameplay.items.weaponUpgrades.WeaponUpgradeFactory;
 import com.lizardwizards.lizardwizards.core.gameplay.items.weaponUpgrades.WeaponUpgrades;
-import com.lizardwizards.lizardwizards.core.gameplay.levels.RoomFactory;
-
-import java.util.Arrays;
 
 public enum ServerCommands {
+    HELP ("help") {
+        @Override
+        public String description() {
+            return "Prints information about server commands";
+        }
+
+        @Override
+        public void execute(String... args) {
+            for (var command : ServerCommands.values()) {
+                var desc = command.description();
+                if (desc != null) {
+                    System.out.printf("%s - %s\n", command.command, desc);
+                }
+            }
+        }
+    },
     START_SERVER ("startServer"){
+        @Override
+        public String description() {
+            return "Starts a new server with the next available port";
+        }
+
         @Override
         public void execute(String... args){
             LizardWizardsServer.startServer();
@@ -26,11 +43,20 @@ public enum ServerCommands {
     },
     SPAWN_MOB ("mob") {
         @Override
+        public String description() {
+            return "(mob <mob id>) Spawns the selected monster";
+        }
+
+        @Override
         public void execute(String... args) {
+            if (args.length < 1) {
+                System.out.println(description());
+                return;
+            }
             EntityWrapper ew;
             Vector2 pos = new Vector2(RoomInformation.xMax / 2, RoomInformation.yMax / 2);
             switch (args[0]) {
-                case "1" -> {
+                case "1" -> { //TODO: there NEEDS to be a better way of doing this
                     var enemy = new BigBugEnemy(pos);
                     var sprite = enemy.getSprite();
                     var w = sprite.getWidth();
@@ -52,14 +78,23 @@ public enum ServerCommands {
     },
     SPAWN_ITEM ("item") {
         @Override
+        public String description() {
+            return "(item <item name>) Spawns the selected item";
+        }
+
+        @Override
         public void execute(String... args) {
+            if (args.length < 1) {
+                System.out.println(description());
+                return;
+            }
             Vector2 pos = new Vector2(RoomInformation.xMax / 2, RoomInformation.yMax / 2);
             for (var upgrade : WeaponUpgrades.values()) {
                 if (upgrade.name().compareToIgnoreCase(args[0]) == 0) {
                     var upgradeFactory = new WeaponUpgradeFactory();
                     var weaponUpgrade = upgradeFactory.getUpgrade(upgrade);
                     var item = new WeaponUpgradeItem(weaponUpgrade);
-                    ItemHolder itemHolder = new ItemHolder(pos, item);
+                    ItemHolder itemHolder = new ItemHolder(pos, item); //TODO: same as above
                     EntitySprite sprite = item.getSprite();
                     Collider collider = Collider.NewRectangle(pos, item.getSize().x, item.getSize().y, CollisionLayer.Item);
                     EntityWrapper ew = new EntityWrapper(itemHolder, sprite, collider);
@@ -70,7 +105,8 @@ public enum ServerCommands {
             System.out.println("Could not find item " + args[0]);
         }
     },
-    NOT_FOUND ("") { // Has to be last in list
+    // Teleporting to specific rooms could be nice, but don't want to fuck with Level right now
+    NOT_FOUND ("") { // Has to be last defined enumerator
         @Override
         public boolean checkMatch(String command) {
             this.command = command;
@@ -90,5 +126,8 @@ public enum ServerCommands {
 
     public void execute(String... args) {
         System.out.println("Could not find command " + this.command);
+    }
+    public String description() {
+        return null;
     }
 }
