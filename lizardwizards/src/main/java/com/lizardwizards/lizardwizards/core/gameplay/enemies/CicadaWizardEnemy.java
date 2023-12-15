ca4f86929db847025112a2914ea5e7cb1abb8535
Povilas <povilassc@gmail.com>
2023-12-15 05:44:05 +0200

    Added a sprite to the Cicada Wizard boss

--- lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/CicadaWizardEnemy.java
+++ lizardwizards/src/main/java/com/lizardwizards/lizardwizards/core/gameplay/enemies/CicadaWizardEnemy.java
@@ -2,20 +2,53 @@ package com.lizardwizards.lizardwizards.core.gameplay.enemies;
 
 import com.lizardwizards.lizardwizards.client.sprites.EntitySprite;
 import com.lizardwizards.lizardwizards.client.sprites.ImageSprite;
+import com.lizardwizards.lizardwizards.client.sprites.SpriteSheet;
 import com.lizardwizards.lizardwizards.core.Vector2;
 import com.lizardwizards.lizardwizards.core.gameplay.Entity;
+import com.lizardwizards.lizardwizards.core.gameplay.Utils;
 import com.lizardwizards.lizardwizards.core.gameplay.collision.CollisionLayer;
-import com.lizardwizards.lizardwizards.core.gameplay.enemies.state.DefaultStateChanger;
 import com.lizardwizards.lizardwizards.core.gameplay.enemies.state.HealthStateChanger;
 import com.lizardwizards.lizardwizards.core.gameplay.enemies.state.TimedStateChanger;
 import com.lizardwizards.lizardwizards.core.gameplay.enemies.strategy.*;
 import com.lizardwizards.lizardwizards.core.gameplay.weapons.ViolentWand;
 
+import java.io.Serializable;
+import java.util.function.Supplier;
+
 public class CicadaWizardEnemy extends BossEnemy {
     final double maxHealth;
     final double maxSpeed;
+    EntitySprite stage1Sprite;
+    EntitySprite stage2Sprite;
+    EntitySprite stage3Sprite;
+    EntitySprite stage4Sprite;
+    EntitySprite sprite;
     public CicadaWizardEnemy(Vector2 position) {
         super(200, 200, position);
+        stage1Sprite = new ImageSprite("images/enemies/Cicada1.png");
+        stage1Sprite.scale(2);
+        stage2Sprite = new ImageSprite("images/enemies/Cicada2.png");
+        stage2Sprite.scale(2);
+        stage3Sprite = new ImageSprite("images/enemies/Cicada3.png");
+        stage3Sprite.scale(2);
+        stage4Sprite = new ImageSprite("images/enemies/Cicada4.png");
+        stage4Sprite.scale(2);
+        sprite = new SpriteSheet(()-> stage1Sprite);
+        ((SpriteSheet)sprite).updateSupplier((Supplier<EntitySprite> & Serializable)() -> {
+            var hp = ((Enemy)Utils.getEntityList().get(uuid).entity).getHealth();
+            if (hp < 50) {
+                return stage4Sprite;
+            }
+            if (hp < 150) {
+                return stage3Sprite;
+            }
+            if (hp < 180) {
+                return stage2Sprite;
+            }
+            return stage1Sprite;
+        });
+
+
         maxHealth = health;
         maxSpeed = speed;
         speed *= 0.5;
@@ -43,6 +76,6 @@ public class CicadaWizardEnemy extends BossEnemy {
 
     @Override
     public EntitySprite getSprite() {
-        return new ImageSprite("images/enemies/BigBug.png");
+        return sprite;
     }
 }
