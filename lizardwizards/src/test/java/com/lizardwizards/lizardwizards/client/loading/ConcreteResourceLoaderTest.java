e74dc169e6a561f9a6315be1ca101e7d3e93ac28
Faustas Linkevičius <faulin@ktu.lt>
2023-12-21 15:16:38 +0200

    jtest testing

---
+++ lizardwizards/src/test/java/com/lizardwizards/lizardwizards/client/loading/ConcreteResourceLoaderTest.java
@@ -0,0 +1,38 @@
+package com.lizardwizards.lizardwizards.client.loading;
+
+
+import org.junit.jupiter.api.Test;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+
+
+/**
+ * Parasoft Jtest UTA: Test class for ConcreteResourceLoader
+ *
+ * @see ConcreteResourceLoader
+ * @author Legion
+ */
+public class ConcreteResourceLoaderTest
+{
+    /**
+     * Parasoft Jtest UTA: Test for loadResources()
+     *
+     * @author Legion
+     * @see ConcreteResourceLoader#loadResources()
+     */
+    @Test
+    public void testLoadResources() throws Throwable {
+        // UTA is unable to resolve the values required to create the requested test case.
+        // A test case with default values has been created instead.
+
+        assertTimeoutPreemptively(ofMillis(5000), () -> {
+            // Given
+            ConcreteResourceLoader underTest = new ConcreteResourceLoader();
+
+            // When
+            underTest.loadResources();
+
+        });
+    }
+}
